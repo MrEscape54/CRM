c8b3e94b869d051988e6751ff040d7a1be52d25f
MrEscape <mrescape@gmail.com>
2020-12-19 21:46:02 -0300

    config

--- crm/settings.py
+++ crm/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,6 +145,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR / 'media/')
 
+STATIC_ROOT = (BASE_DIR / 'staticfiles')
+
+#Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # drf-spectacular
 REST_FRAMEWORK = {
